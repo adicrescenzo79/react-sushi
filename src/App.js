53661260520c1bbbc02d06fc494924dfe8210bda
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-07-22 12:55:32 +0200

    completo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,100 @@
+import React, {Component} from 'react';
+
 import Navbar from './components/navbar';
 import Card from './components/card';
+import shrimp from './img/shrimp.png';
+import california from './img/california.png';
+import dragon from './img/dragon.png';
+import dynamite from './img/dynamite.png';
+import philadelphia from './img/philadelphia.png';
+import rainbow from './img/rainbow.png';
 
+class App extends Component {
+  state = {
+    cards: [
+      {
+        id: 0,
+        nome: 'California',
+        prezzo: 1.99,
+        immagine: california,
+        quantita: 0,
+      },
+      {
+        id: 1,
+        nome: 'dragon',
+        prezzo: 1.99,
+        immagine: dragon,
+        quantita: 0,
+      },
+      {
+        id: 2,
+        nome: 'dynamite',
+        prezzo: 1.99,
+        immagine: dynamite,
+        quantita: 0,
+      },
+      {
+        id: 3,
+        nome: 'philadelphia',
+        prezzo: 1.99,
+        immagine: philadelphia,
+        quantita: 0,
+      },
+      {
+        id: 4,
+        nome: 'rainbow',
+        prezzo: 1.99,
+        immagine: rainbow,
+        quantita: 0,
+      },
+      {
+        id: 5,
+        nome: 'shrimp',
+        prezzo: 1.99,
+        immagine: shrimp,
+        quantita: 0,
+      },
 
-function App() {
-  return (
-    <>
+    ]
+  }
+  handleDelete = cardId => {
+    const cards = this.state.cards.filter(card => card.id !== cardId);
+    this.setState({cards});
+  }
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = {...card};
+    cards[id].quantita++;
+    this.setState({cards});
+
+  }
+  render(){
+
+    return (
+      <>
       <Navbar />
       <div className="container">
 
-        <h1>Cosa desideri ordinare?</h1>
-        <hr />
-        <div className="row">
-          <Card />
-        </div>
+      <h1>Cosa desideri ordinare?</h1>
+      <hr />
+      <div className="row">
+        {
+          this.state.cards.map(card => (
+            <Card
+              key={card.id}
+              onDelete={this.handleDelete}
+              onIncrement={this.handleIncrement}
+              card={card}
+            />
+          ))
+
+        }
       </div>
-    </>
-  );
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,24 @@
-function Card(){
-  return (
-    <div classNameName="col">
-      <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+import React, {Component} from 'react';
 
-    </div>
-  );
+
+class Card extends Component {
+  render(){
+
+    return (
+      <div className="col">
+        <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
+        <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">Aggiungi <span className="badge badge-light">{this.props.card.quantita}</span></button>
+          <img src={this.props.card.immagine} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.card.nome} Roll</h5>
+            <p className="card-text">€ {this.props.card.prezzo}</p>
+            <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default Card;
