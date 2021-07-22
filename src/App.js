a678ba210f7cf4446399fe1ea4da1e86e8afb062
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-07-22 10:45:06 +0200

    primo commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/navbar';
+import Card from './components/card';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+
+        <h1>Cosa desideri ordinare?</h1>
+        <hr />
+        <div className="row">
+          <Card />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
